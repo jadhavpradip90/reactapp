b6b5b9050cd18316d5ba7801c685ab41538c71b3
jadhavpradip90 <jadhavpradip90@gmail.com>
2018-03-31 01:38:26 +0530

    Props and children setup

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,12 +1,30 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 export class Home extends React.Component{
     render() {
+        console.log(this.props);
         return(
             <div>
                 <p>In a new component</p>
-                { 2 + 2}
+                <p>Your name is {this.props.name}, your age is {this.props.age}</p>
+                <p>Your object => Name: {this.props.user.name}</p>
+                <div>
+                    <h4>Hobbies</h4>
+                    <ul>
+                        {this.props.user.hobbies.map((hobby, index) => <li key={index}>{hobby}</li>)}
+                    </ul>
+                </div>
+                <hr/>
+                {this.props.children}
             </div>
         );
     }
 }
+
+Home.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    user: PropTypes.object,
+    children: PropTypes.element.isRequired
+};
--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,10 @@ import { Home } from "./components/Home";
 
 class App extends React.Component {
     render() {
+        var user = {
+          name: "Vikas",
+          hobbies: ["Sports", "Music", "Travelling"]
+        };
         return(
             <div className="container">
                 <div className="row">
@@ -14,7 +18,9 @@ class App extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home/>
+                        <Home name={"Pradip"} age={35} user={user}>
+                            <p>This is home children</p>
+                        </Home>
                     </div>
                 </div>
             </div>
